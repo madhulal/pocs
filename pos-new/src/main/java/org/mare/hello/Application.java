9a465128a996458c4d709a885b8f27a8081ce6cd
madhulal <madhulal@mozanta.com>
2015-10-04 19:16:44 +0530

    Added spring boot pos project

---
+++ pos-new/src/main/java/org/mare/hello/Application.java
@@ -0,0 +1,16 @@
+package org.mare.hello;
+
+/**
+ * Created by Madhulal M G on 4/10/15.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ pos-new/src/main/java/org/mare/hello/MvcConfig.java
@@ -0,0 +1,22 @@
+package org.mare.hello;
+
+/**
+ * Created by Madhulal M G on 4/10/15.
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
